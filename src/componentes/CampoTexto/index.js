1aeb241ba1ddbe17f2f1167b9ec9c4b9edb83e46
Hideaki Fukami <denys.fukami@gmail.com>
2023-01-15 15:08:35 -0300

    Correction

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,17 +1,28 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
 
+    // let valor = ""
+
+    // Todo use é hookie. useState retorna uma array de um valor e uma função de definição.
+    // Dessa forma você separa os componentes da array de retorno.
+
+
+    const [valor, setValor] = useState('')
+
     const aoDigitado = (evento) => {
-        let valor = evento.target.value
+        setValor(evento.target.value)
         console.log(valor)
     }
 
     return (
         <div className="campo-texto">
-            <label>{props.label}</label>
+            <label>
+                {props.label}
+                </label>
             <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada} />
         </div>
     )
